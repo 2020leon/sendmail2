55dcac168c20eaafd6ce123fa057e59a661831a1
2020leon <leon2000.tw@gmail.com>
2022-09-17 23:24:50 +0800

    refactor: use await syntax

--- src/sender.ts
+++ src/sender.ts
@@ -4,27 +4,17 @@ import SendmailError from './error/sendmail_error';
 import sendToSMTP from './send_to_smtp';
 
 export default class Sender {
-  public send(options: import('nodemailer/lib/mailer').Options) {
+  public async send(options: import('nodemailer/lib/mailer').Options) {
     const mail = new MailComposer(options);
     const compiled = mail.compile();
     const envelope = compiled.getEnvelope();
     const senderAddress = envelope.from;
     if (!senderAddress) throw new SendmailError('sender address is invalid');
-    return new Promise<void>((resolve, reject) => {
-      compiled.build((err, msg) => {
-        if (err) return reject(err);
-        return Promise.all(
-          envelope.to.map((address) => {
-            try {
-              return sendToSMTP(senderAddress, address, msg);
-            } catch (e) {
-              return Promise.reject(e);
-            }
-          }),
-        )
-          .then(() => resolve())
-          .catch(reject);
-      });
-    });
+    const body = await new Promise<Buffer>((resolve, reject) =>
+      compiled.build((err, msg) => (err ? reject(err) : resolve(msg))),
+    );
+    await Promise.all(
+      envelope.to.map((address) => sendToSMTP(senderAddress, address, body)),
+    );
   }
 }
